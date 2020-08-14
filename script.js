aab988ed5e2b14a8b83cd06dae79eb622aa42d62
Maria <maria.betancourt15@yahoo.com>
2020-08-14 15:29:30 -0700

    linked script js

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock
-// THEN I can enter an event
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+
+setInterval(function () {
+  $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+}, 1000);
+
+let schedule = localStorage.getItem("schedule");
+if (schedule) {
+  schedule = JSON.parse(schedule);
+} else {
+  schedule = {};
+}
+
+for (id in schedule) {
+  $("#" + id)
+    .children("textarea")
+    .val(schedule[id]);
+}
+
+$(".saveBtn").on("click", function (e) {
+  const val = $(this).siblings("textarea")[0].value.trim();
+  const id = $(this).parent().attr("id");
+
+  schedule[id] = val;
+
+  localStorage.setItem("schedule", JSON.stringify(schedule));
+});
+
+const currentHour = parseInt(moment().format("H"));
+
+$(".time-block").each(function () {
+  const hour = parseInt($(this).attr("id").replace("hour-", ""));
+  console.log(hour);
+  if (hour < currentHour) {
+    $(this).addClass("past");
+  } else if (currentHour === hour) {
+    $(this).addClass("present");
+  } else {
+    $(this).addClass("future");
+  }
+});
